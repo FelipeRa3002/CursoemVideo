357428784300ec29a16a5ff3090a8ddef8655dba
FelipeRa3002 <136349846+FelipeRa3002@users.noreply.github.com>
2025-12-25 19:57:26 +0000

    Terminei o módulo 5

--- Html e CSS3/Projeto portfolio/scripts/script.js
+++ Html e CSS3/Projeto portfolio/scripts/script.js
@@ -1,6 +1,8 @@
 const botao = document.getElementById('botao-tema');
 const body = document.body;
 
+// Detecta o modo escuro do navegador
+const ModoEscuroDoNavegadorAtivado = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 // Persistência do tema
 const temasalvo = localStorage.getItem('tema');
 temaEscuro(temasalvo === 'escuro');
@@ -10,18 +12,33 @@ function temaClaroAtivo() {
   // Implemente a lógica para ativar o tema claro
   let MudarCordosBotoes = document.querySelectorAll(".botoes-menu");
     MudarCordosBotoes.forEach(botoes => {
-      botoes.style.color = 'black';
+      botoes.style.color = 'white';
     });
+}
+// Função para ativar o tema escuro
+function temaEscuroAtivo() {
+  // Implemente a lógica para ativar o tema escuro
+  let MudarCordosBotoes = document.querySelectorAll(".botoes-menu");
+  MudarCordosBotoes.forEach(botoes => {
+    botoes.style.color = 'black';
+  });
+}
+// Aplica o tema escuro se o modo escuro do navegador estiver ativado ao carregar a página
+if (ModoEscuroDoNavegadorAtivado) {
+  body.classList.add('escuro');
+  botao.innerHTML = '<abbr title="Modo Claro"><i class="fa-solid fa-sun botoes-menu" style="color: black;"  onmouseenter="this.style.color=\'yellow\'" onmouseleave="this.style.color=\'black\'"></i></abbr>';
+  temaEscuroAtivo();
 }
 // Função para alternar entre tema claro e escuro
 function temaEscuro(tipo) {
   if (tipo == true) {
     body.classList.add('escuro');
     botao.innerHTML = '<abbr title="Modo Claro"><i class="fa-solid fa-sun botoes-menu" style="color: black;"  onmouseenter="this.style.color=\'yellow\'" onmouseleave="this.style.color=\'black\'"></i></abbr>';
-    temaClaroAtivo();
+    temaEscuroAtivo();
   } else {
     body.classList.remove('escuro');
-    botao.innerHTML = '<abbr title="Modo Escuro"><i class="fa-solid fa-moon botoes-menu"></i></abbr>';
+    botao.innerHTML = '<abbr title="Modo Escuro"><i class="fa-solid fa-moon botoes-menu" style="color: black;"  onmouseenter="this.style.color=\'black\'" onmouseleave="this.style.color=\'white\'"></i></abbr>';
+    temaClaroAtivo();
   }
 }
 
